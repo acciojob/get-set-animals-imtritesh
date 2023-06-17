c38f413ed23198f0100e0ff3433cb234210b7a89
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-06-17 13:45:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ class Dog extends Animal {
 
 class Cat extends Animal {
 	purr() {
-	    console.log(`The ${this.species()} makes a sound purr`);
+	    console.log(`The ${this.species()} makes a sound ${this.purr()}`);
 	}
 
 	makeSound() {
